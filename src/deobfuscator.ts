4f97acf063ada51601a2dd5d39c6931aa8effc7e
Rafael Piovesan <rubens.piovesan@gmail.com>
2022-08-08 23:19:26 -0300

    feature: use prettier parse

--- src/deobfuscator.ts
+++ src/deobfuscator.ts
@@ -1,6 +1,7 @@
 import { writeFileSync, mkdirSync } from "fs";
 import { deobfuscate } from "js-deobfuscator";
 import { logger } from "./logger.js";
+import { format } from "prettier";
 
 export type Script = {
   script: string;
@@ -42,17 +43,23 @@ export function deobfuscateScript(param: Script): void {
   }
 
   try {
-    const output = deobfuscate(param.script, deobfuscationConfig);
-    param.script = output;
+    param.script = deobfuscate(param.script, deobfuscationConfig);
   } catch (e) {
     param.filename = `original_${param.filename}`;
     logger.warn(`could not deobfuscate script from [${param.hostname}]`, e);
   }
 
   try {
+    param.script = format(param.script, {parser: "babel-flow", useTabs: false, tabWidth: 2, printWidth: 80});
+  } catch (e) {
+    logger.warn(`could not format script from [${param.hostname}]`);
+  }
+
+  try {
     const saveToDir = `output/${param.webpage}/${param.hostname}`;
+    const saveToFile = `${saveToDir}/${param.filename.replace(/.js$/, "")}.js`
     mkdirSync(saveToDir, { recursive: true });
-    writeFileSync(`${saveToDir}/${param.filename.replace(/.js$/, "")}.js`, param.script);
+    writeFileSync(saveToFile, param.script);
   } catch(e) {
     logger.error(`could not save script [${param.filename}] from [${param.hostname}]`, e)
   }
